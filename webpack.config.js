d1a733ec35746e57b3728d11b5fd53702e0bf12c
Darren Lewis <darren@octono.com>
2018-10-14 14:35:25 +0100

    Added publicPath to ensure webpack-dev-server can see dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,13 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/dist'
     },
     module: {
         rules: [
             { test: /\.css$/, use: 'css-loader' },
             { test: /\.ts$/, use: 'ts-loader' }
         ]
-    }//,
-    // devServer: {
-    //     //contentBase: path.join(__dirname, 'dist'),
-    //     //compress: true,
-    //     port: 8080
-    //   }
+    }
 }
